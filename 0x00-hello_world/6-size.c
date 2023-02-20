8dae9f23579e9dd40e1e2360a55bec72705c8669
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-02-20 15:05:34 -0800

    6th-task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	char character;
+	int integer;
+	long longInt;
+	long long longLong;
+	float afloat;
+
+	printf("Size of a char: %lu bytes(s)"\n, sizeof(character));
+	printf("Size of an int: %lu bytes(s)"\n, sizeof(integer));
+	printf("Size of a long int: %lu bytes(s)"\n, sizeof(longInt));
+	printf("Size of a long long: %lu bytes(s)"\n, sizeof(longLong));
+	printf("Size of a float: %lu bytes(s)"\n, sizeof(afloat));
+
+	return (0);
+}	
