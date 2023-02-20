7c505c0ba4df8459724a5bab76cd1dd09806f2f6
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-02-20 15:28:22 -0800

    6th task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,8 +17,8 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(character));
 	printf("Size of an int: %lu byte(s)\n", sizeof(integer));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longInt));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(longLong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longLong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
 	return (0);
-}	
+}
