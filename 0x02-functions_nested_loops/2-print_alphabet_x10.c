aa447c77d125baee827f766a0199137a2501d4ff
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-02-22 16:46:48 -0800

    task 2'

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lowercase ten times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int newAlphas;
+
+	for (newAlphas = 0; newAlphas < 10; newAlphas++)
+	{
+		print_alphabet();
+	}
+}
