3e9302d744d978314395a6efaff0857917f7af33
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-02-23 17:20:17 -0800

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,15 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if ((a > b) && (b > c))
+    if (a >= b && b >= c)
     {
         largest = a;
     }
-    if ((b > a) && (a > c))
+    else if (b >= a && b >= c)
     {
         largest = b;
     }
-    if ((c > a) && ((a > b)) 
+    else
     {
         largest = c;
     }
