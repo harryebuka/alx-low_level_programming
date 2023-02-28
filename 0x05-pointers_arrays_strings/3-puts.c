e6239d584fdefda8ab7d15ad100fe0ed7ee7d64a
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-02-28 15:23:04 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ *_puts - prints a string, to stdout
+ *@str: string value to be evaluated
+ *Return: nothing.
+ */
+void _puts(char *str)
+{
+	int length;
+
+	length = 0;
+
+	while (*(str + 1) != '\0')
+	{
+		_putchar(str[1]);
+		length++;
+	}
+	_putchar('\n');
+}
