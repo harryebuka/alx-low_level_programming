32648013f5dc534413a1efb7a8af539ab941b6f8
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-02-28 15:58:19 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *puts_half - update value
+ *@str: string value to be evaluated
+ *Return: nothing
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	char *y = str;
+	int n;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1) / 2;
+	}
+	for ( ; n < len ; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
