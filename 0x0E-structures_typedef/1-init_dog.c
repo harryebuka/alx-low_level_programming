332b8f9df85c3d5c330edae09958ba32970fac4d
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-03-21 16:58:42 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,39 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * struct dog - a struct representing a dog
- * @name: the dog's name
- * @age: the dog's age
- * @owner: the dog's owner
- */
-struct dog
-{
-    char name[50];
-    float age;
-    char owner[50];
-};
-
-/**
- * init_dog - initializes a struct of type dog with the provided values
- * @d: pointer to the dog struct to be initialized
- * @name: the dog's name
- * @age: the dog's age
- * @owner: the dog's owner
- */
+ * init_dog - initialize a variable of type struct dog
+ * @d: pointer to an element of type dog
+ * @name: pointer to a char for name of dog
+ * @age: age of dog
+ * @owner: pointer to a char for owner of dog
+ *
+ * Return: nothing
+ **/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
-    if (name == NULL)
-        d->name[0] = '\0';
-    else
-        strncpy(d->name, name, sizeof(d->name));
-    d->age = age;
-    if (owner == NULL)
-        d->owner[0] = '\0';
-    else
-        strncpy(d->owner, owner, sizeof(d->owner));
+	if (d == NULL)
+		;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
