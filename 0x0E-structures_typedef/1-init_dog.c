88cc47950557a1b6c88272a508f5f2a0e2b31cbd
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-03-21 16:56:51 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -22,12 +22,18 @@ struct dog
  * @name: the dog's name
  * @age: the dog's age
  * @owner: the dog's owner
- *
- * Return: nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	strcpy(d->name, name);
-	d->age = age;
-	strcpy(d->owner, owner);
+    if (d == NULL)
+        return;
+    if (name == NULL)
+        d->name[0] = '\0';
+    else
+        strncpy(d->name, name, sizeof(d->name));
+    d->age = age;
+    if (owner == NULL)
+        d->owner[0] = '\0';
+    else
+        strncpy(d->owner, owner, sizeof(d->owner));
 }
