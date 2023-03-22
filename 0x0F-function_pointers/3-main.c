5e6cd44e757e05d5619a64ac66a5960b8f7cb937
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-03-22 14:21:10 -0700

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - searches an integer
+ * @array: pointer array
+ * @size: number elements in array
+ * @cmp: is a pointer to the function to be used to compare values
+ * Return: first element index,on succes or return -1
+ **/
+
+int main(void)
+{
+}
