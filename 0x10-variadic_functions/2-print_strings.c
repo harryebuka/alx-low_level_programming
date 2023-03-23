4085325d9699ce5700f6ae2b2af4c33fd4aec322
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-03-23 06:08:34 -0700

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - printsstrings followed by a new line
+ * @separator: seperators
+ * @n: number of arguements
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i;
+	char *p;
+
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		p = va_arg(strings, char *);
+		if (p == NULL)
+			printf("(nil)");
+		else
+		{
+			printf("%s", p);
+			if (i < n - 1 && separator != NULL)
+				printf("%s", separator);
+		}
+	}
+	va_end(strings);
+	printf("\n");
+}
