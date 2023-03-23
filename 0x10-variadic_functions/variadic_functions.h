f6f2a299e6325b5fd281c59cfde7c7963348e7ce
Ebuka Harry <harry.chukwuebuka.harry@gmail.com>
2023-03-23 06:25:16 -0700

    added prototypes to functions in task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,8 +5,15 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_i(va_list list, char *s);
-void print_c(va_list list, char *sep);
-void print_s(va_list list, char *sep);
-void print_f(va_list list, char *sep);
+/**
+ * struct typ - Struct operators
+ *
+ * @c: Char
+ * @tp: The function associated
+ */
+typedef struct typ
+{
+	char *c;
+	void (*tp)(va_list var);
+} typ_t;
 #endif
